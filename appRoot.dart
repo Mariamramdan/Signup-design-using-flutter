d5c365a86127e265d7d75c799adb56d4f3cb3d1d
Mariam <101593782+Mariamramdan@users.noreply.github.com>
2023-09-05 20:38:47 +0300

    Create appRoot.dart

---
+++ appRoot.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+import '../Screens/SecondScreen.dart';
+import '../Screens/homeScreen1.dart';
+
+class MyApp extends StatelessWidget {  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomeScreen(),
+    );
+  }
+}
